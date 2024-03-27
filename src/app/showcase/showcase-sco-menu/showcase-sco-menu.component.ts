72f016b0bed86973f800214f11f3258e3809275f
scomeras94 <scomeras@gmail.com>
2024-03-27 14:42:48 +0100

    ADD - SCO Menu Showcase

--- src/app/showcase/showcase-sco-menu/showcase-sco-menu.component.ts
+++ src/app/showcase/showcase-sco-menu/showcase-sco-menu.component.ts
@@ -1,4 +1,39 @@
+import { MenuItem } from './../../../../projects/sco-angular-components/src/components/sco-menu/model/menu-item';
 import { Component, OnInit } from '@angular/core';
+
+export const EXAMPLE_MENU: MenuItem[] = [
+  {
+    icon: "fa fa-caret-down",
+    text: "Gest. usuarios",
+    children: [
+      {
+        text: "Usuarios",
+        route: "users",
+        icon: "fa fa-user"
+      },
+      {
+        text: "Roles",
+        route: "roles",
+        icon: "fa fa-user"
+      },
+      {
+        text: "Permisos",
+        route: "permissions",
+        icon: "fa fa-user"
+      }
+    ]
+  },
+  {
+    icon: "fa fa-gear",
+    text: "Opciones",
+    route: "/options",
+  },
+  {
+    icon: "fa fa-sign-out",
+    text: "Cerrar sesión",
+    route: "/auth/logout",
+  },
+];
 
 @Component({
   selector: 'app-showcase-sco-menu',
@@ -7,9 +42,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowcaseScoMenuComponent implements OnInit {
 
-  constructor() { }
+  public menuItems: MenuItem[];
+  public borderRadius: boolean;
+
+  constructor() { 
+    this.menuItems = [];
+    this.borderRadius = false;
+  }
 
   ngOnInit() {
+    this.menuItems = EXAMPLE_MENU;
   }
 
 }
