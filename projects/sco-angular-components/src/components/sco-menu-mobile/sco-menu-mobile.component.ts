d0eef4058d5eadd1aa1234d58b05e058df5cfc2a
scomeras94 <scomeras@gmail.com>
2024-04-10 02:44:41 +0200

    UPD - Mobile Menu Funcional & Themes

--- projects/sco-angular-components/src/components/sco-menu-mobile/sco-menu-mobile.component.ts
+++ projects/sco-angular-components/src/components/sco-menu-mobile/sco-menu-mobile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output, ViewEncapsulation } from '@angular/core';
+import { MenuItem } from './../sco-menu/model/menu-item';
+import { ScoConstantsService } from './../../services/sco-constants.service';
 
 @Component({
   selector: 'sco-menu-mobile',
@@ -6,11 +8,82 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./sco-menu-mobile.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ScoMenuMobileComponent implements OnInit {
+export class ScoMenuMobileComponent {
 
-  constructor() { }
+  @Input() menuItems: MenuItem[] = [];
 
-  ngOnInit() {
+  @Input() menuButtonTransparent: boolean = true;
+  @Input() menuButtonIcon: string = 'fa fa-bars';
+
+  @Input() showMenuIcons: boolean = true;
+  @Input() translatePipe: boolean = false;
+  @Input() headerMargin: number = 0;
+
+  @Output() itemClick: EventEmitter<MenuItem>;
+
+  public showMobileMenu: boolean;
+  public selectedMenuItem: string
+  public submenuItems: MenuItem[];
+  public forceCloseMobileMenu: boolean;
+  public preventContentClick: boolean;
+
+  public windowHeight: number;
+
+  constructor(
+    public readonly constantsService: ScoConstantsService,
+  ) {
+    this.itemClick = new EventEmitter<MenuItem>();
+
+    this.showMobileMenu = false;
+    this.selectedMenuItem = '';
+    this.submenuItems = [];
+    this.forceCloseMobileMenu = false;
+    this.preventContentClick = false;
+
+    this.windowHeight = window.innerHeight;
+   }
+
+  onClickMobileMenu($event: boolean) {
+    if (!$event || this.showMobileMenu) return;
+
+    this.showMobileMenu = !this.showMobileMenu;
+    this.selectedMenuItem = '';
+    this.submenuItems = [];
+    this.forceCloseMobileMenu = false;
+    this.preventContentClick = false;
   }
 
+  onCLickMobileMenuItem($event: MenuItem) {
+    if (!$event) return;
+
+    this.selectedMenuItem = $event.text;
+    this.submenuItems = [];
+    this.preventContentClick = true;
+
+    if ($event.children && $event.children.length > 0) {
+      for (const menuItem of $event.children) {
+        this.submenuItems.push(menuItem);
+      }
+    } else {
+      this.itemClick.emit($event);
+      this.forceCloseMobileMenu = true;
+    }
+  }
+
+  onCloseMobileMenu() {
+    this.showMobileMenu = false;
+  }
+
+  onClickMobileMenuContent() {
+    if (this.preventContentClick) {
+      return;
+    }
+
+    this.forceCloseMobileMenu = true;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.windowHeight = event.target.innerHeight;
+  }
 }
--- projects/sco-angular-components/src/components/sco-menu-mobile/sco-menu-mobile.module.ts
+++ projects/sco-angular-components/src/components/sco-menu-mobile/sco-menu-mobile.module.ts
@@ -2,11 +2,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScoMenuMobileComponent } from './sco-menu-mobile.component';
+import { ScoButtonModule } from './../sco-button/sco-button.module';
+import { ScoDetailModule } from './../sco-detail/sco-detail.module';
+import { ScoTranslateModule } from './../../services/sco-translate/sco-translate.module';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserAnimationsModule,
+    ScoButtonModule,
+    ScoDetailModule,
+    ScoTranslateModule,
   ],
   declarations: [
     ScoMenuMobileComponent,
