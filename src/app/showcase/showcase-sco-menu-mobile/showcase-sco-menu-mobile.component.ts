d524fc896014731c7762af975b1aa0a923326dcf
scomeras94 <scomeras@gmail.com>
2024-04-10 03:06:41 +0200

    UPD - SCO Menu Mobile showcase

--- src/app/showcase/showcase-sco-menu-mobile/showcase-sco-menu-mobile.component.ts
+++ src/app/showcase/showcase-sco-menu-mobile/showcase-sco-menu-mobile.component.ts
@@ -1,4 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from './../../../../projects/sco-angular-components/src/components/sco-menu/model/menu-item';
+
+export const EXAMPLE_MENU: MenuItem[] = [
+  {
+    icon: "fa fa-caret-down",
+    text: "Gest. usuarios",
+    children: [
+      {
+        text: "Usuarios",
+        route: "users",
+        icon: "fa fa-user"
+      },
+      {
+        text: "Roles",
+        route: "roles",
+        icon: "fa fa-user"
+      },
+      {
+        text: "Permisos",
+        route: "permissions",
+        icon: "fa fa-user"
+      }
+    ]
+  },
+  {
+    icon: "fa fa-gear",
+    text: "Opciones",
+    route: "/options",
+  },
+  {
+    icon: "fa fa-sign-out",
+    text: "Cerrar sesión",
+    route: "/auth/logout",
+  },
+];
 
 @Component({
   selector: 'app-showcase-sco-menu-mobile',
@@ -7,9 +42,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowcaseScoMenuMobileComponent implements OnInit {
 
-  constructor() { }
+  public menuItems: MenuItem[];
+
+  constructor() { 
+    this.menuItems = [];
+  }
 
   ngOnInit() {
+    this.menuItems = EXAMPLE_MENU;
+  }
+
+  onClickMenuMobileItem($event: MenuItem) {
+
   }
 
 }
