d0f5d4cc9e8928038e3135df2736977702f4d687
scomeras94 <scomeras@gmail.com>
2024-04-12 17:05:54 +0200

    ADD -dropdown setSelectedItem doc

--- src/app/showcase/showcase-sco-dropdown/showcase-sco-dropdown.component.ts
+++ src/app/showcase/showcase-sco-dropdown/showcase-sco-dropdown.component.ts
@@ -1,3 +1,4 @@
+import { ScoConstantsService } from 'projects/sco-angular-components/src/public-api';
 import { ScoCacheService } from '../../../../projects/sco-angular-components/src/services/sco-cache.service';
 import { ScoToastService } from './../../../../projects/sco-angular-components/src/components/sco-toast/sco-toast.service';
 import { ScoSelectItem } from '../../../../projects/sco-angular-components/src/common/sco-select-item';
@@ -15,10 +16,13 @@ export class ShowcaseScoDropdownComponent<T> implements OnInit {
   public valueEx2: any;
   public valueEx3: any;
   public valueEx5: any;
+  public valueEx6: any;
+  public setSelectedItemEx6: any;
 
   constructor(
     private readonly toastService: ScoToastService,
-    private readonly cacheService: ScoCacheService
+    private readonly cacheService: ScoCacheService,
+    public readonly constantsService: ScoConstantsService,
   ) { 
     this.cacheService.setElement("title", "Dropdown");
   }
@@ -34,9 +38,16 @@ export class ShowcaseScoDropdownComponent<T> implements OnInit {
     this.valueEx2 = "";
     this.valueEx3 = "";
     this.valueEx5 = "Value4";
+    this.valueEx6 = "Value2";
+    this.setSelectedItemEx6 = undefined;
   }
 
   select($event) {
     this.toastService.addInfoMessage("Éxito", `Has elegido: ${JSON.stringify($event)}`);
+  }
+
+  onSetSelectedItemEx6() {
+    this.valueEx6 = "Value3";
+    this.setSelectedItemEx6 = this.options.find(o => o.value == this.valueEx6);
   }
 }
