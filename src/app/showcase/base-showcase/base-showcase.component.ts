a8bce8a0e6be2d1254375bbe6a46611d543aa6d9
Santiago Comeras Oteo <santiagocomerasoteo@gmail.com>
2024-07-21 04:01:11 +0200

    ADD - Titlte property

--- src/app/showcase/base-showcase/base-showcase.component.ts
+++ src/app/showcase/base-showcase/base-showcase.component.ts
@@ -1,20 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'base-showcase',
   templateUrl: './base-showcase.component.html',
   styleUrls: ['./base-showcase.component.scss']
 })
-export class BaseShowcaseComponent implements OnInit {
+export class BaseShowcaseComponent {
 
+  @Input() title: string = undefined;
   @Input() showEamples: boolean = true;
   @Input() showCode: boolean = true;
   @Input() showInputs: boolean = true;
   @Input() showOutputs: boolean = true;
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
